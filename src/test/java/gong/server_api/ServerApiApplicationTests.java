de1d75d8b8a53fb39057dd01b51bb0320a0714f8
sunghyun <a91078933@gmail.com>
2024-05-25 23:32:53 +0900

    Initial commit

---
+++ src/test/java/gong/server_api/ServerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package gong.server_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServerApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
