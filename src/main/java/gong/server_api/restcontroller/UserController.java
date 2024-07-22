be4e977ea29ed0f942b956c3c0c99a29c9a87552
sunghyun <a91078933@gmail.com>
2024-07-22 23:18:37 +0900

    //역할이 소방관인 유저는 병원 목록만 뜨게 한다.     //역할이 병원인 유저는 소방관 목록만 뜨게 한다.     //jwt에서 role을 추출해서 role에 따라서 접속상태 확인     //자신을 제외한 접속자 나오게

--- src/main/java/gong/server_api/restcontroller/UserController.java
+++ src/main/java/gong/server_api/restcontroller/UserController.java
@@ -1,10 +1,12 @@
 package gong.server_api.restcontroller;
 
 
+import gong.server_api.domain.dto.UserDto;
 import gong.server_api.domain.dto.UserJoinDto;
+import gong.server_api.repository.UserRepository;
+import gong.server_api.service.HospitalConnectionStatusService;
 import gong.server_api.service.UserService;
-import gong.server_api.service.response.UserLogin;
-import jakarta.servlet.http.HttpServletRequest;
+import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,18 +18,35 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     private final UserService userService;
+    private final UserRepository userRepository;
 
-    public UserController(UserService userService) {
+    private final HospitalConnectionStatusService hospitalConnectionStatusService;
+    public UserController(UserService userService, UserRepository userRepository, HospitalConnectionStatusService hospitalConnectionStatusService) {
         this.userService = userService;
+        this.userRepository = userRepository;
+        this.hospitalConnectionStatusService = hospitalConnectionStatusService;
     }
 
     //회원가입 컨트롤러
-    // 회원가입 컨트롤러
-    @PostMapping("/api/v1/join")
-    public ResponseEntity<String> join(@RequestBody UserJoinDto userJoinDto){
-        log.info("userJoinDto ={}", userJoinDto);
-        userService.userJoin(userJoinDto);
-        return ResponseEntity.ok("회원가입이 성공적으로 완료되었습니다.");
+    @PostMapping("/join")
+    public ResponseEntity<?> joinUser(@RequestBody @Valid UserJoinDto userJoinDto) {
+        try {
+            userService.userJoin(userJoinDto);
+            return ResponseEntity.ok("User successfully registered");
+        } catch (IllegalArgumentException e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
+
+    @GetMapping("/user")
+    public UserDto findUserInfo() {
+        return userService.findUserImfo();
+    }
+
+    //역할이 소방관인 유저는 병원 목록만 뜨게 한다.
+    //역할이 병원인 유저는 소방관 목록만 뜨게 한다.
+    //jwt에서 role을 추출해서 role에 따라서 접속상태 확인
+    //자신을 제외한 접속자 나오게
+
 
 }
