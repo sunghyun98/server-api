c7ab62ee9ca23aec8026c030882cd8c53c033e68
sunghyun <a91078933@gmail.com>
2024-07-29 20:26:54 +0900

    jwt 인터셉터 추가

--- src/main/java/gong/server_api/WebSocketConfig.java
+++ src/main/java/gong/server_api/WebSocketConfig.java
@@ -1,6 +1,7 @@
 package gong.server_api;
 
 import gong.server_api.handler.ChatWebSocketHandler;
+import gong.server_api.handler.HeaderValidationInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
@@ -12,16 +13,17 @@ import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry
 public class WebSocketConfig implements WebSocketConfigurer {
 
     private final ChatWebSocketHandler chatWebSocketHandler;
-
+    private final HeaderValidationInterceptor headerValidationInterceptor;
     @Autowired
-    public WebSocketConfig(ChatWebSocketHandler chatWebSocketHandler) {
+    public WebSocketConfig(ChatWebSocketHandler chatWebSocketHandler, HeaderValidationInterceptor headerValidationInterceptor) {
         this.chatWebSocketHandler = chatWebSocketHandler;
-
+        this.headerValidationInterceptor = headerValidationInterceptor;
     }
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
         registry.addHandler(chatWebSocketHandler, "/ws/chat")
+                .addInterceptors(headerValidationInterceptor)
                 .setAllowedOrigins("*");
     }
 }
