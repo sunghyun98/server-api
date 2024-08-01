31729ed373b08b4e880d9ec49f917ea9bdca47bc
sunghyun <a91078933@gmail.com>
2024-08-01 22:58:48 +0900

    mybatis 추가

---
+++ src/main/java/gong/server_api/MyBatisConfig.java
@@ -0,0 +1,29 @@
+package gong.server_api;
+
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.ClassPathResource;
+
+import javax.sql.DataSource;
+
+@Configuration
+@MapperScan("gong.server_api.mapperRepo") // 지정된 패키지에서 매퍼 인터페이스를 스캔합니다.
+public class MyBatisConfig {
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
+        sessionFactory.setDataSource(dataSource);
+        sessionFactory.setConfigLocation(new ClassPathResource("mybatis-config.xml"));
+        return sessionFactory.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
